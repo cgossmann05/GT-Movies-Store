ae071374819e749917fa12067e52875eb04381c4
cgossmann05 <cgossmann05@gmail.com>
2025-08-25 22:33:42 -0400

    Update background image to use external URL

--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.shortcuts import redirect
 from .forms import CustomUserCreationForm, CustomErrorList
-from django.contrib.auth import login as auth_login, authenticate
+from django.contrib.auth import login as auth_login, authenticate, logout as auth_logout
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 
